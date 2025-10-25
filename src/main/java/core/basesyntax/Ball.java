24d023e43dedadcc23f7a0c617a2ea1b80348ba2
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-10-25 19:09:50 +0200

    Fix Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -2,8 +2,32 @@ package core.basesyntax;
 
 public class Ball {
 
-    Colors color;
-    int number;
+    private Colors color;
+    private int number;
+
+    public Ball() {
+    }
+
+    public Ball(Colors color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     @Override
     public String toString() {
@@ -11,4 +35,5 @@ public class Ball {
         sb.append("Ball with the number: ").append(number).append(" and color: ").append(color);
         return sb.toString();
     }
+
 }
